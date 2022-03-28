15fbac318a479d254b6df2353e102f05c0295ac0
arunprksm <97307361+arunprksm@users.noreply.github.com>
2022-03-28 22:49:20 +0530

    Start button audio added using Fmod

--- React It 3D/Assets/_Projects/Scripts/AudioManager.cs
+++ React It 3D/Assets/_Projects/Scripts/AudioManager.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private SoundType[] Sounds;
+    //public EventReference eventReference;
+    //[SerializeField] private string sfxEvent = null;
+    public void PlaySFX(Sounds sound)
     {
-        
+        SoundType item = Array.Find(Sounds, i => i.soundType == sound);
+        if (item.sfxEvent != null)
+        {
+            item.sfxEvent = item.eventReference.Path;
+            RuntimeManager.PlayOneShot(item.sfxEvent);
+        }
+        else
+        {
+            Debug.LogError("Clip not found on soundType: " + sound);
+        }
     }
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[Serializable]
+public class SoundType
+{
+    public Sounds soundType;
+    public EventReference eventReference;
+    public string sfxEvent = null;
+}
+public enum Sounds
+{
+    ButtonClick,
+    LevelSelection,
+    Music,
+    PlayerMove,
+    PlayerDeath
+    //EnemyDeath,
+    //GameMusic_scene1,
 }
--- React It 3D/Assets/_Projects/Scripts/MainMenuController.cs
+++ React It 3D/Assets/_Projects/Scripts/MainMenuController.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    private AudioManager audioManager;
+
+    private void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
     public void PlayButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        audioManager.PlaySFX(Sounds.ButtonClick);
     }
 }
